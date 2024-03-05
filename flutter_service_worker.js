9ed1f6c7b0f7e45fb99eda64601f3b5d4da068de
Tambong Stercy <tambongsterling@gmail..com>
2024-03-05 20:14:34 +0100

    error messages display 3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -779,9 +779,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "8a50ac1a83e9dbf829f7d3170f9aa77c
 "icons/Icon-512.png": "6819edd30cd590bfc347332015edbc57",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "3df75e28cdd54bc682579d39abba202f",
-"/": "3df75e28cdd54bc682579d39abba202f",
-"main.dart.js": "168ea3e231a18908c08f784aceb1cfd6",
+"index.html": "cd1953c2aad5e112e60cc1c0801e6086",
+"/": "cd1953c2aad5e112e60cc1c0801e6086",
+"main.dart.js": "314895978fee098e78422edc5f9c8ee0",
 "manifest.json": "ad499f2577fe7083952529c29d17cf47",
 "version.json": "3c061d9ed92cadbbef1b8ab50ecdda34"};
 // The application shell files that are downloaded before a service worker can
