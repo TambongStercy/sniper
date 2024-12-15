651c699d07c14e7b8b23ca30464682f96cc28eb0
TambongStercy <stercytambong@gmail.com>
2024-12-15 04:17:06 +0100

    ARRANGED FEW ENPOINT ERRORS

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -791,14 +791,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "cf4d8d6b0496115ebe8db7376904800f
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "6819edd30cd590bfc347332015edbc57",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
-"flutter_bootstrap.js": "f853dc3657292e52760e15ad3740fdd0",
+"flutter_bootstrap.js": "25c32a803260b0e76551fcdd6faf9d71",
 "icons/Icon-192.png": "6819edd30cd590bfc347332015edbc57",
 "icons/Icon-512.png": "6819edd30cd590bfc347332015edbc57",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "63220ce060fc4723dcf70c5548419e1b",
-"/": "63220ce060fc4723dcf70c5548419e1b",
-"main.dart.js": "07b6ebc021d5b2cc5315a4739a8689ce",
+"index.html": "26250a7bed11a665e96e2c96c592378a",
+"/": "26250a7bed11a665e96e2c96c592378a",
+"main.dart.js": "b1e5eca3b9235c921879ca34c1a25e3e",
 "manifest.json": "ad499f2577fe7083952529c29d17cf47",
 "version.json": "3c061d9ed92cadbbef1b8ab50ecdda34"};
 // The application shell files that are downloaded before a service worker can
