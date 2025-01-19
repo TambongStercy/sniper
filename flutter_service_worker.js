e54253f93ba95dcb3bf4ee872ac8057c7bfb3bc1
TambongStercy <stercytambong@gmail.com>
2025-01-19 23:45:06 +0100

    redirect fieul to whatsapp

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -791,14 +791,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "cf4d8d6b0496115ebe8db7376904800f
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "6819edd30cd590bfc347332015edbc57",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
-"flutter_bootstrap.js": "ada92ecf5972537c43be9c8910c4b486",
+"flutter_bootstrap.js": "cdcff10297427139c4ca7c2f5fb2633f",
 "icons/Icon-192.png": "6819edd30cd590bfc347332015edbc57",
 "icons/Icon-512.png": "6819edd30cd590bfc347332015edbc57",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "084b43758e31e9cba4f9453e1ec9da1d",
-"/": "084b43758e31e9cba4f9453e1ec9da1d",
-"main.dart.js": "91f0563bb81a41993c71159c996325aa",
+"index.html": "0080cc2b2d8fd36631f025bd96a947b7",
+"/": "0080cc2b2d8fd36631f025bd96a947b7",
+"main.dart.js": "f75860306335fe1fed94fd18ced155d5",
 "manifest.json": "ad499f2577fe7083952529c29d17cf47",
 "version.json": "3c061d9ed92cadbbef1b8ab50ecdda34"};
 // The application shell files that are downloaded before a service worker can
