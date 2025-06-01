3491964488b4efa012ab23336734849845611554
TambongStercy <stercytambong@gmail.com>
2025-06-01 10:17:52 +0100

    product changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1342262312"
+    serviceWorkerVersion: "438316785"
   }
 });
